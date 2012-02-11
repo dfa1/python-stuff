b9c7a6a43c170ded6a5b89cd2be8f26543b13911
Davide Angelocola <davide.angelocola@gmail.com>
2012-02-11 20:17:26 +0100

    enh: new mac address script

---
+++ newmacaddr.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import subprocess
+import io
+import sys
+
+class Command(object):
+
+    def __init__(self, path):
+        self.path = path
+
+    def __call__(self, *args):
+        call_args = list(args)
+        call_args.insert(0, self.path)
+        return subprocess.check_call(call_args)
+
+def octect(i): 
+    return "{0:02x}".format(i)
+
+def random_macaddr():
+    from random import randint
+    random_bytes = [0] + [randint(0, 256) for x in range(5)]
+    random_octects = map(octect, random_bytes)
+    return ":".join(random_octects)
+
+if __name__ == '__main__':
+    ifname = "wlan0"
+    if len(sys.argv) == 2:
+        ifname = sys.argv[1]
+    ifconfig = Command("/sbin/ifconfig") 
+    new_macaddr = random_macaddr()
+    ifconfig(ifname, "down") 
+    ifconfig(ifname, "hw", "ether", new_macaddr)
+    ifconfig(ifname, "up")
+    print "{0} mac address now is {1}".format(ifname, new_macaddr)
